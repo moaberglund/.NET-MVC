5a9fc91441ca7d9160e3814ba838e6bbbad9bcb5
moaberglund <moa.21@hotmail.com>
2025-02-06 16:43:02 +0100

    Start on model

--- MVC/Models/BookModel.cs
+++ MVC/Models/BookModel.cs
@@ -1,4 +1,12 @@
 ﻿namespace MVC.Models
 {
-
+    public class BookModel
+    {
+        // Properties
+        public required string Title { get; set; }
+        public required string Author { get; set; }
+        public required int Year { get; set; }
+        public int rating { get; set; }
+        public bool haveRead { get; set; }
+    }
 }
