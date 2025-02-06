b95e3f3a869e09fe0faa3611099c5401a7f1da31
moaberglund <moa.21@hotmail.com>
2025-02-06 17:22:47 +0100

    adding validation

--- MVC/Models/BookModel.cs
+++ MVC/Models/BookModel.cs
@@ -1,10 +1,23 @@
-﻿namespace MVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models
 {
     public class BookModel
     {
         // Properties
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(50)]
         public required string BookTitle { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(50)]
         public required string Author { get; set; }
+
+        [Required]
+        [Range(1, 2025)]
         public required int Year { get; set; }
         public int Rating { get; set; }
         public bool HaveRead { get; set; }
